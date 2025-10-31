2268ae3e358af475d87598f9819227fcb2b1e6d6
kathdiao <katherinejoycediao22@gmail.com>
2025-10-31 11:08:07 +0800

    Added mini quiz game

---
+++ quiz_game/quiz_game.py
@@ -0,0 +1,53 @@
+print("Welcome to Quiz Game!")
+question = input("Do you want to play?")
+score = 0
+
+if question.lower() == "yes":
+    print("Let's play")
+    answer = input("What does RAM stand for? ")
+    if answer.lower() == "random access memory":
+        score += 1
+        print("Correct!")
+        print(f"Score: {score}/5")
+    else:
+        print("Incorrect!")
+        print(f"Score: {score}/5")
+
+    answer = input("What does ROM stand for? ")
+    if answer.lower() == "read only memory":
+        score += 1
+        print("Correct!")
+        print(f"Score: {score}/5")
+    else:
+        print("Incorrect!")
+        print(f"Score: {score}/5")
+
+    answer = input("What does CPU stand for? ")
+    if answer.lower() == "central processing unit":
+        score += 1
+        print("Correct!")
+        print(f"Score: {score}/5")
+    else:
+        print("Incorrect!")
+        print(f"Score: {score}/5")
+
+    answer = input("What does SSD stand for? ")
+    if answer.lower() == "solid state drive":
+        score += 1
+        print("Correct!")
+        print(f"Score: {score}/5")
+    else:
+        print("Incorrect!")
+        print(f"Score: {score}/5")
+
+    answer = input("What does HDD stand for? ")
+    if answer.lower() == "hard disk drive":
+        score += 1
+        print("Congrats, you got it!")
+        print(f"Total Score: {score}/5")
+    else:
+        print("Incorrect!")
+        print(f"Score: {score}/5")
+else:
+    print("Exiting...")
+    quit()
