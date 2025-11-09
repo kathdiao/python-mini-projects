8ae2d9b715eedd9d71f975e9a8aa230433d761f7
kathdiao <katherinejoycediao22@gmail.com>
2025-11-09 12:40:56 +0800

    Add multiplication table generator

---
+++ multiplication_table_generator/multiplication_table.py
@@ -0,0 +1,10 @@
+try:
+    number_to_generate = int(input("Enter the number to generate: "))
+    max_multiplier = int(input("Enter the max multiplier: "))
+
+    for i in range(1, max_multiplier + 1):
+        result = number_to_generate * i
+        print(number_to_generate,"x", i, "=", result)
+
+except ValueError:
+    print("Should be an integer")
