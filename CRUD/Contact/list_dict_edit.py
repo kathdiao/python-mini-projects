0108c366bba8ff83bc7e9dc4b87557d72f515298
kathdiao <katherinejoycediao22@gmail.com>
2025-11-26 16:14:30 +0800

    Add contact editing: name and phone

--- CRUD/Contact/list_dict_edit.py
+++ CRUD/Contact/list_dict_edit.py
@@ -1,23 +1,44 @@
-#  Focused on editing phone number inside list dictionaries
+#    Simple contact editor focused on editing phone numbers and names
 contacts = [
     {"name": "Katherine", "phone": "09923654701"},
     {"name": "Tala", "phone": "09923654702"},
     {"name": "Jea", "phone": "09923654703"}
 ]
 
-for item, value in enumerate(contacts, start=1):
-    print(f"{item}. {value['name']} : {value['phone']}")
+for items, value in enumerate(contacts, start=1):
+    print(f"{items}. {value['name']} : {value['phone']}")
 
-# yung name yung icacall and change ng phone number
-edit = input("Enter contact name you want to change: ").title()
+print("EDIT Contact")
+print("1. Edit Phone number")
+print("2. Edit Name")
+print("3. Exit")
 
-#   run loop to check isa isa yung nasa loob ng list_dict
-for i in range(len(contacts)):
-    if contacts[i]['name'] == edit:
-        new_phone = input("Enter new phone number: ")
-        contacts[i]['phone'] = new_phone
-        print("Contact changed successfully")
+user_input = input("Enter your choice 1/2?: ")
 
-        for item, value in enumerate(contacts, start=1):
-            print(f"{item}. {value['name']} : {value['phone']}")
+if user_input == "1":
+    edit = input("Enter contact name you want to change: ").title()
 
+    for i in range(len(contacts)):
+        if contacts[i]['name'] == edit:
+            new_phone = input("Enter new phone number: ")
+            contacts[i]['phone'] = new_phone
+            print("Contact changed successfully")
+
+            for item, val in enumerate(contacts, start=1):
+                print(f"{item}. {val['name']} : {val['phone']}")
+
+elif user_input == "2":
+
+    edit_name = input("What name would you like to change? ").title()
+
+    for i in range(len(contacts)):
+        if edit_name == contacts[i]["name"]:
+            new_name = input("Enter new name: ").title()
+            contacts[i]["name"] = new_name
+            print("Name updated successfully!")
+
+            for t, v in enumerate(contacts, start=1):
+                print(f"{t}: {v['name']} : {v['phone']}")
+
+elif user_input == "3":
+    quit()
